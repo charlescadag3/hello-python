a03887c737d922f48ea1495b47a28de5fa7c58d0
Charles Cadag <charlescadag3@gmail.com>
2018-10-26 01:03:17 +0800

    Excercise 3

---
+++ Cadag_e3.py
@@ -0,0 +1,10 @@
+
+UserIn = float(input("Enter a comma separated list of numbers :"))
+values =UserIn.split(",")
+print("The number you enter are{}".format(values[0],values[1],values[2],values[3],values[4])
+values ** 2
+
+total = value1+value2+value3+value4
+
+
+print ("Sum of Squares"+values)
---
+++ loop.py
@@ -0,0 +1,26 @@
+# x = 1
+# sum = 0
+# while x < 100:
+#     if x & 1:
+#         print(x, end =" ")
+#         sum += x
+#     x+=1
+
+# print ("Total" +str(sum))
+
+# for c in "Hello!":
+#     print (c)
+# for p in range (5,100):
+#     if p % 2 == 0:
+#         print(p, end = " ")
+# print ()
+
+# numbers = []
+# for x in range(10):
+#     numbers.append(x)
+# print (numbers)
+
+# numbers2 = [ x for x in range(10)]
+# print (numbers2)
+numbers2 = [ x**2 for x in range(10)]
+print (numbers2)
