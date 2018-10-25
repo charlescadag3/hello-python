805974d9bd29b78014f70ea7e62ada2dcc644a26
Charles Cadag <charlescadag3@gmail.com>
2018-10-26 00:00:31 +0800

    Excercise 2

--- Cadag_e2.py
+++ Cadag_e2.py
@@ -1,17 +1,17 @@
 
 
-UserIn = int(input("Please type the number of sustained wind in km/h:."))
+UserIn = float(input("Please type the number of sustained wind in km/h:"))
 
 
-if UserIn>=220:
+if UserIn>=220.0:
  print ("Super Typhoon")
-elif UserIn>=118 and UserIn <=220:
+elif UserIn>=118.0 and UserIn <=220.0:
  print ("Typhoon")
-elif UserIn>=89 and UserIn<=117:
+elif UserIn>=89.0 and UserIn<=117.0:
  print ("Severe Tropical Storm")
-elif UserIn>=62 and UserIn<=88:
+elif UserIn>=62.0 and UserIn<=88.0:
  print ("Tropical Storm")
-elif  UserIn>=0 and UserIn<=61: 
- print ("Severe Tropical Storm")
+elif  UserIn>=1. and UserIn<=61.0: 
+ print ("Tropical Depression")
  
 
